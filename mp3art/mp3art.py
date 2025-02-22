d66c6fc81341c5755be0c978d4ae0e8e3adb0ea8
Ryn. <rynstwrt17@gmail.com>
2025-02-22 02:24:42 -0600

    Update mp3art.py

--- mp3art/mp3art.py
+++ mp3art/mp3art.py
@@ -2,21 +2,49 @@ import argparse
 from pathlib import Path
 
 
-def validate_path(path):
+VALID_AUDIO_EXTENSIONS = [".mp3"]
+VALID_IMAGE_EXTENSIONS = [".png"]
+
+
+
+def validate_path(file_path: Path, is_audio_path: bool):
+    path_type = "MP3" if is_audio_path else "Cover art"
+
+    if not file_path.is_file():
+        print(f'{path_type} path "{file_path}" was not found or is not a file!')
+        return False
+
+    valid_extensions = VALID_AUDIO_EXTENSIONS if is_audio_path else VALID_IMAGE_EXTENSIONS
+    file_extension = file_path.suffix.lower()
+    if file_extension not in valid_extensions:
+        print(f'{path_type} file has invalid extension "{file_extension}"! Valid extensions: {", ".join(valid_extensions)}.')
+        return False
+
     return True
+
 
 
 def add_cover(args):
     mp3_path = args.mp3
     cover_path = args.cover
     print(mp3_path, cover_path)
-    # print("adding cover %s to %s".format(cover_path, mp3_path))
+
+    if not validate_path(mp3_path, True) or not validate_path(cover_path, False):
+        return False
+
+    print("AAAA")
+
 
 
 def remove_cover(args):
     mp3_path = args.mp3
     print(mp3_path)
-    # print("removing cover from %s".format(mp3_path))
+
+    if not validate_path(mp3_path, True):
+        return False
+
+    print("A")
+
 
 
 def run():
